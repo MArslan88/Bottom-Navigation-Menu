9e9691418cc5426955d551a7a7c4a58e2a1cb612
MArslan88 <gery_maz_555@hotmail.com>
2022-09-27 14:01:20 +0500

    simple version updated

---
+++ app/src/main/java/com/mhdarslan/bottomnavigation/MainActivity.java
@@ -0,0 +1,49 @@
+package com.mhdarslan.bottomnavigation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    FirstFragment firstFragment = new FirstFragment();
+    SecondFragment secondFragment = new SecondFragment();
+    ThirdFragment thirdFragment = new ThirdFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,firstFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected( MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.firstFragment:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,firstFragment).commit();
+                        return true;
+                    case R.id.secondFragment:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,secondFragment).commit();
+                        return true;
+                    case R.id.thirdFragment:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,thirdFragment).commit();
+                        return true;
+
+
+                }
+                return false;
+            }
+        });
+
+    }
+}
